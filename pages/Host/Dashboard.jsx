5c8baac4fed97da0190a9b350a35700fe0ffd55f
Bronze14 <marcindadek@gmail.com>
2023-05-07 23:27:59 +0200

    delete console.log

--- pages/Host/Dashboard.jsx
+++ pages/Host/Dashboard.jsx
@@ -9,7 +9,7 @@ export function loader() {
 
 export default function Dashboard() {
     const loaderData = useLoaderData()
-
+    console.log(loaderData)
     function renderVanElements(vans) {
         const hostVansEls = vans.map((van) => (
             <div className="host-van-single" key={van.id}>
